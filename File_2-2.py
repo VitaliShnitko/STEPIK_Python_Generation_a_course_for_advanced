29cfe0efd4f7c305beb40477848673b4076ecc54
Vitali Shnitko <stigmator565@gmail.com>
2023-01-04 22:22:14 +0300

    Finish File_2-2.py

--- File_2-2.py
+++ File_2-2.py
@@ -3,20 +3,20 @@
 # лежащих в каждой координатной четверти.
 
 
-# number = int(input())
-# first, second, third, fourth = 0, 0, 0, 0
-#
-# for _ in range(number):
-#     x, y = map(int, input().split())
-#     first += x > 0 and y > 0
-#     second += x < 0 and y > 0
-#     third += x < 0 and y < 0
-#     fourth += x > 0 and y < 0
-#
-# print(f"Первая четверть: {first}")
-# print(f"Вторая четверть: {second}")
-# print(f"Третья четверть: {third}")
-# print(f"Четвертая четверть: {fourth}")
+number = int(input())
+first, second, third, fourth = 0, 0, 0, 0
+
+for _ in range(number):
+    x, y = map(int, input().split())
+    first += x > 0 and y > 0
+    second += x < 0 and y > 0
+    third += x < 0 and y < 0
+    fourth += x > 0 and y < 0
+
+print(f"Первая четверть: {first}")
+print(f"Вторая четверть: {second}")
+print(f"Третья четверть: {third}")
+print(f"Четвертая четверть: {fourth}")
 
 
 
@@ -25,13 +25,13 @@
 # которые больше предшествующего им в этом списке числа,
 # то есть, стоят вслед за меньшим числом.
 
-# num = [int(i) for i in input().split()]
-# count = 0
-#
-# for i in range(1, len(num)):
-#     if int(num[i]) > int(num[i-1]):
-#         count += 1
-# print(count)
+num = [int(i) for i in input().split()]
+count = 0
+
+for i in range(1, len(num)):
+    if int(num[i]) > int(num[i-1]):
+        count += 1
+print(count)
 
 
 # На вход программе подается строка текста из натуральных чисел.
@@ -41,10 +41,10 @@
 # Если в списке нечетное количество элементов,
 # то последний остается на своем месте.
 
-# num = input().split()
-# for i in range(0, len(num)-1, 2):
-#     num[i], num[i+1] = num[i+1], num[i]
-# print(*num)
+num = input().split()
+for i in range(0, len(num)-1, 2):
+    num[i], num[i+1] = num[i+1], num[i]
+print(*num)
 
 
 # На вход программе подается строка текста из натуральных чисел.
@@ -53,10 +53,10 @@
 # когда последний элемент становится первым,
 # а остальные сдвигаются на одну позицию вперед, в сторону увеличения индексов.
 
-# num = input().split()
-#
-# num2 = num[-1:] + num[:-1]
-# print(*num2)
+num = input().split()
+
+num2 = num[-1:] + num[:-1]
+print(*num2)
 
 
 # На вход программе подается строка текста,
@@ -64,12 +64,12 @@
 # Из строки формируется список чисел.
 # Напишите программу для подсчета количества разных элементов в списке.
 
-# num = input().split()
-# count = 1
-# for i in range(0, len(num)-1):
-#     if num[i] != num[i+1]:
-#         count += 1
-# print(count)
+num = input().split()
+count = 1
+for i in range(0, len(num)-1):
+    if num[i] != num[i+1]:
+        count += 1
+print(count)
 
 
 # Напишите программу для определения, является ли число произведением двух чисел из данного набора,
@@ -80,21 +80,21 @@
 # В последующих nn строках вводятся целые числа, составляющие набор (могут повторяться).
 # Затем следует целое число, которое является или не является произведением двух каких-то чисел из набора.
 
-# n = int(input())
-# a = [input() for i in range(n)]
-# pr = int(input())
-# count = 0
-#
-# for i in range(len(a)):
-#     for j in range(len(a)):
-#         if int(a[i]) * int(a[j]) == pr and i != j:
-#             count += 1
-#         else:
-#             continue
-# if count > 0:
-#     print('ДА')
-# else:
-#     print('НЕТ')
+n = int(input())
+a = [input() for i in range(n)]
+pr = int(input())
+count = 0
+
+for i in range(len(a)):
+    for j in range(len(a)):
+        if int(a[i]) * int(a[j]) == pr and i != j:
+            count += 1
+        else:
+            continue
+if count > 0:
+    print('ДА')
+else:
+    print('НЕТ')
 
 
 # Тимур и Руслан пытаются разделить фронт работы по курсу "Python для профессионалов".
@@ -103,24 +103,24 @@
 # кто будет делать очередной модуль нового курса.
 
 
-# # 1 решение
-# timur = input()
-# ruslan = input()
+# 1 решение
+timur = input()
+ruslan = input()
+
+if ruslan == timur:
+    print('ничья')
+elif timur == 'камень' and ruslan == 'ножницы':
+    print('Тимур')
+elif timur == 'ножницы' and ruslan == 'бумага':
+    print('Тимур')
+elif timur == 'бумага' and ruslan == 'камень':
+    print('Тимур')
+else:
+    print('Руслан')
 #
-# if ruslan == timur:
-#     print('ничья')
-# elif timur == 'камень' and ruslan == 'ножницы':
-#     print('Тимур')
-# elif timur == 'ножницы' and ruslan == 'бумага':
-#     print('Тимур')
-# elif timur == 'бумага' and ruslan == 'камень':
-#     print('Тимур')
-# else:
-#     print('Руслан')
-#
-# # 2 решение
-# tm, rs = input(), input()
-# print('ничья' if tm == rs else 'Тимур' if tm + rs in 'каменьножницыбумагакамень' else 'Руслан')
+# 2 решение
+tm, rs = input(), input()
+print('ничья' if tm == rs else 'Тимур' if tm + rs in 'каменьножницыбумагакамень' else 'Руслан')
 
 
 # Проиграв 1010 раз Тимуру, Руслан понял, что так дело дальше не пойдет,
@@ -129,17 +129,17 @@
 # Помогите ребятам вновь бросить честный жребий и определить,
 # кто будет делать следующий модуль в новом курсе.
 
-# tm, rs = input(), input()
-# print('ничья' if tm == rs else 'Тимур' if tm + rs in
-# 'каменьножницыбумагакаменьящерицаСпокножницыящерицабумагаСпоккамень' else 'Руслан')
+tm, rs = input(), input()
+print('ничья' if tm == rs else 'Тимур' if tm + rs in
+'каменьножницыбумагакаменьящерицаСпокножницыящерицабумагаСпоккамень' else 'Руслан')
 
 
 # Дана строка текста, состоящая из букв русского алфавита "О" и "Р".
 # Буква "О" – соответствует выпадению Орла, а буква "Р" – соответствует выпадению Решки.
 # Напишите программу, которая подсчитывает наибольшее количество подряд выпавших Решек.
 
-# text = input().split('О')
-# print(len(max(text)))
+text = input().split('О')
+print(len(max(text)))
 
 
 # Искусственный интеллект Антон, созданный Гилфойлом, взломал сеть умных холодильников.
@@ -151,16 +151,16 @@
 # (необязательно рядом стоящие буквы, главное наличие последовательности букв),
 # то холодильник заражен и нужно вывести номер холодильника, нумерация начинается с единицы
 
-# virus = 'anton'
-# for i in range(1, int(input()) + 1):
-#     s = input()
-#     res = ''
-#     for x in virus:
-#         if x in s:
-#             res += x
-#             s = s[s.find(x):]
-#     if res == virus:
-#         print(i, end=' ')
+virus = 'anton'
+for i in range(1, int(input()) + 1):
+    s = input()
+    res = ''
+    for x in virus:
+        if x in s:
+            res += x
+            s = s[s.find(x):]
+    if res == virus:
+        print(i, end=' ')
 
 
 # Необходимо написать программу, реализующую алгоритм написания этой песни.
