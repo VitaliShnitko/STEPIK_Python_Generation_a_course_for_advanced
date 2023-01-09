572b0c6db432cbf4be0d19607fa2f917de8c70ec
Vitali Shnitko <stigmator565@gmail.com>
2023-01-09 20:52:08 +0300

    Added solution 2 task.

--- File_4-3.py
+++ File_4-3.py
@@ -2,11 +2,21 @@
 # которая создает и выводит построчно список,
 # состоящий из nn списков [[1, 2, ..., n], [1, 2, ..., n], ..., [1, 2, ..., n]].
 
-n = int(input())    # считываем значения n
+# n = int(input())    # считываем значения n
+#
+# for i in range(n):
+#     my_list = []
+#     for j in range(1, n+1):
+#         my_list.append(j)
+#     print(my_list)
+# print()
 
-for i in range(n):
-    my_list = []
-    for j in range(1, n+1):
-        my_list.append(j)
-    print(my_list)
-print()
+
+# На вход программе подается число nn. Напишите программу,
+# которая создает и выводит построчно вложенный список,
+# состоящий из nn списков [[1], [1, 2], [1, 2, 3], ..., [1, 2, ..., n]].
+
+n = int(input())    # считываем значения n
+my_list = [list(range(1, i+1)) for i in range(1, n + 1)]
+for row in my_list:
+    print(row)
