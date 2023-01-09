15dfe0059c69094ba1bdabc08246d13ec10cecbf
Vitali Shnitko <stigmator565@gmail.com>
2023-01-09 21:25:46 +0300

    Added solution 4 task.

--- File_4-3.py
+++ File_4-3.py
@@ -26,24 +26,38 @@
 # В этом треугольнике на вершине и по бокам стоят единицы.
 # Каждое число равно сумме двух расположенных над ним чисел.
 
-# 1 решение
-n = int(input())
-lst = [[1]]
-for i in range(1, n + 1):
-    row = [1] * (i + 1)
-    for j in range(i + 1):
-        if j != 0 and j != i:
-            row[j] = lst[i - 1][j - 1] + lst[i - 1][j]
-    lst.append(row)
+# # 1 решение
+# n = int(input())
+# lst = [[1]]
+# for i in range(1, n + 1):
+#     row = [1] * (i + 1)
+#     for j in range(i + 1):
+#         if j != 0 and j != i:
+#             row[j] = lst[i - 1][j - 1] + lst[i - 1][j]
+#     lst.append(row)
+#
+# print(lst[-1])
+#
+# # 2 решение
+# from math import factorial
+# n = int(input())
+# b = []
+# for i in range(n+1):
+#     b.append(int((factorial(n))/(factorial(i)*factorial(n-i))))
+# print(b)
 
-print(lst[-1])
 
-# 2 решение
+# На вход программе подается натуральное число n.
+# Напишите программу, которая выводит первые nn строк треугольника Паскаля.
+
 from math import factorial
 n = int(input())
-b = []
-for i in range(n+1):
-    b.append(int((factorial(n))/(factorial(i)*factorial(n-i))))
-print(b)
-
+for i in range(0, n):
+    b = []
+    for j in range(0, n):
+        if i < j:
+            break
+        else:
+            b.append(int((factorial(i))/(factorial(j)*factorial(i-j))))
+    print(*b)
 
