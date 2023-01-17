d1eee87dc2b6d022f04dddac563fec83a41a68a1
Vitali Shnitko <stigmator565@gmail.com>
2023-01-17 19:50:02 +0300

    Added solution 2 task.

--- File_4-5_Matrix-2.py
+++ File_4-5_Matrix-2.py
@@ -2,9 +2,27 @@
 # Создайте матрицу mult размером n \times mn×m и
 # заполните её таблицей умножения по формуле mult[i][j] = i * j.
 
+# n, m = int(input()), int(input())
+# for i in range(n):
+#     mult = []
+#     for j in range(m):
+#         mult.append(str(i * j).ljust(3))
+#     print(*mult)
+
+
+# На вход программе подаются два натуральных числа n и m — количество строк и столбцов в матрице,
+# затем n строк по m целых чисел в каждой, отделенных символом пробела.
+#
+# Напишите программу, которая находит индексы (строку и столбец)
+# первого вхождения максимального элемента.
+
 n, m = int(input()), int(input())
+matrix = [[int(i) for i in input().split()] for _ in range(n)]
+row, col = 0, 0
+
 for i in range(n):
-    mult = []
     for j in range(m):
-        mult.append(str(i * j).ljust(3))
-    print(*mult)
+        if matrix[i][j] > matrix[row][col]:
+            row, col = i, j
+print(row, col)
+
