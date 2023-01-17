fd657bc3fb66a8fee0699a55b372a7f40d415f8b
Vitali Shnitko <stigmator565@gmail.com>
2023-01-17 21:49:32 +0300

    Added solution 9 task.

--- File_4-5_Matrix-2.py
+++ File_4-5_Matrix-2.py
@@ -116,20 +116,50 @@
 # отметьте английской буквой N, клетки, которые бьет конь,
 # отметьте символами *, остальные клетки заполните точками.
 
-x,y = input()
-n = 8
-matrix = [['.'] * n for _ in range(n)]
-y = n - int(y)
-x = ord(x) - 97
-matrix[y][x] = 'N'
+# x,y = input()
+# n = 8
+# matrix = [['.'] * n for _ in range(n)]
+# y = n - int(y)
+# x = ord(x) - 97
+# matrix[y][x] = 'N'
+#
+# for i in range(n):
+#     for q in range(n):
+#         if abs(i - y) * abs(q - x) == 2:
+#             matrix[i][q] = '*'
+#
+# for x in range(n):
+#     print(*matrix[x])
 
+
+# Магическим квадратом порядка n называется квадратная таблица размера n×n,
+# составленная из всех чисел 1, 2, 3,…,n
+# так, что суммы по каждому столбцу, каждой строке и каждой из двух диагоналей равны между собой.
+# Напишите программу, которая проверяет,
+# является ли заданная квадратная матрица магическим квадратом.
+
+n = int(input())
+matrix = []
+
+for _ in range(n):
+    temp = [int(x) for x in input().split()]
+    matrix.append(temp)
+digits = [i for i in range(1, n**2 +1)]
+
+d1, d2 = 0, 0
 for i in range(n):
-    for q in range(n):
-        if abs(i - y) * abs(q - x) == 2:
-            matrix[i][q] = '*'
-
-for x in range(n):
-    print(*matrix[x])
-
-
+    stolb_sum, stroka_sum = 0, 0
+    for j in range(n):
+        stolb_sum += matrix[j][i]
+        stroka_sum += matrix[i][j]
+        if matrix[i][j] in digits:
+            digits.remove(matrix[i][j])
+    d1 += matrix[i][i]
+    d2 += matrix[i][n-i-1]
+    if stolb_sum != stroka_sum:
+        break
+if stolb_sum == stroka_sum == d1 == d2 and digits == []:
+    print('YES')
+else:
+    print('NO')
 
