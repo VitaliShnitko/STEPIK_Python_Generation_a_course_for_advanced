b28f124942ec27e7759c83098c87999fd1a9cd20
Vitali Shnitko <stigmator565@gmail.com>
2023-01-25 20:56:42 +0300

    Added solution 9 task.

--- File_4-6_Matrix-3.py
+++ File_4-6_Matrix-3.py
@@ -193,25 +193,49 @@ n×m заполнив её в соответствии с образцом.
 '''
 
 
+# n, m = [int(i) for i in input().split()]
+# matrix = [[0] * m for _ in range(n)]
+#
+# count = 1
+#
+# for i in range(n):
+#     for j in range(m):
+#         matrix[i][j] = count
+#         count += 1
+#
+# for i in range(n):
+#     if i % 2 != 0:
+#         matrix[i].reverse()
+#
+# for i in range(n):
+#     for j in range(m):
+#         print(str(matrix[i][j]).ljust(3), end=' ')
+#     print()
+
+
+'''
+Заполнение диагоналями
+На вход программе подаются два натуральных числа 
+n и m. Напишите программу, которая создает матрицу размером 
+n×m заполнив её "диагоналями" в соответствии с образцом.
+'''
+
+
 n, m = [int(i) for i in input().split()]
 matrix = [[0] * m for _ in range(n)]
 
 count = 1
-
-for i in range(n):
-    for j in range(m):
-        matrix[i][j] = count
-        count += 1
-
-for i in range(n):
-    if i % 2 != 0:
-        matrix[i].reverse()
+for k in range(n * m):
+    for i in range(n):
+        for j in range(m):
+            if i + j == k:
+                matrix[i][j] = count
+                count += 1
 
 for i in range(n):
     for j in range(m):
         print(str(matrix[i][j]).ljust(3), end=' ')
     print()
-
 
 
 
